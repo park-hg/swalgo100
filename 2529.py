f49748028fad142429b02d10c6225c89f7d6b45d
park-hg <jogatpark@gmail.com>
2022-07-07 03:25:57 +0900

    20220707

--- 2529.py
+++ 2529.py
@@ -4,19 +4,23 @@ k = int(sys.stdin.readline())
 ineqs = sys.stdin.readline().split()
 visited = [False]*10
 nums = []
+
 def backtrack(d, num):
-    print(num)
     if len(num) == k+1:
         nums.append(num)
         return
 
     for i in range(10):
         if not visited[i]:
-            if not num or eval(num[-1] + ineqs[d] + str(i)):
+            if eval(num[-1] + ineqs[d] + str(i)):
                 visited[i] = True
                 backtrack(d+1, num+str(i))
                 visited[i] = False
 
+for i in range(10):
+    visited[i] = True
+    backtrack(0, str(i))
+    visited[i] = False
 
-backtrack(0, "")
-print(nums)
+print(nums[-1])
+print(nums[0])
